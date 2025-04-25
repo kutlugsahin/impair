25aab6b717ae4be3e904e97ac3efec45a41345f0
sahinkutlu <kutlu@adcreative.ai>
2025-04-26 00:38:50 +0300

    fix toRaw

--- lib/impair/src/utils/toRaw.ts
+++ lib/impair/src/utils/toRaw.ts
@@ -8,13 +8,25 @@ export function toRaw<T>(value: T): T {
   if (typeof value === 'object' && value !== null) {
     if (Array.isArray(value)) {
       return value.map(toRaw) as unknown as T
-    } else {
+    } else if (Object.getPrototypeOf(value) === Object.prototype) {
       const rawValue: Record<string, unknown> = {}
       for (const key in value) {
         if (Object.prototype.hasOwnProperty.call(value, key)) {
           rawValue[key] = toRaw((value as Record<string, unknown>)[key])
         }
       }
+      return rawValue as T
+    } else if (value instanceof Map) {
+      const rawValue = new Map()
+      value.forEach((v, k) => {
+        rawValue.set(k, toRaw(v))
+      })
+      return rawValue as T
+    } else if (value instanceof Set) {
+      const rawValue = new Set()
+      value.forEach((v) => {
+        rawValue.add(toRaw(v))
+      })
       return rawValue as T
     }
   }
