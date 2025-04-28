a7317a1691f0a555c7f2941488252ddba0433f0d
sahinkutlu <kutlu@adcreative.ai>
2025-04-28 12:48:24 +0300

    toRaw keep instance

--- lib/impair/src/utils/toRaw.ts
+++ lib/impair/src/utils/toRaw.ts
@@ -7,27 +7,24 @@ export function toRaw<T>(value: T): T {
 
   if (typeof value === 'object' && value !== null) {
     if (Array.isArray(value)) {
-      return value.map(toRaw) as unknown as T
+      value.forEach((v, i) => {
+        value[i] = toRaw(v)
+      })
     } else if (Object.getPrototypeOf(value) === Object.prototype) {
-      const rawValue: Record<string, unknown> = {}
       for (const key in value) {
-        if (Object.prototype.hasOwnProperty.call(value, key)) {
-          rawValue[key] = toRaw((value as Record<string, unknown>)[key])
-        }
+        value[key] = toRaw(value[key])
       }
-      return rawValue as T
     } else if (value instanceof Map) {
-      const rawValue = new Map()
       value.forEach((v, k) => {
-        rawValue.set(k, toRaw(v))
+        value.set(k, toRaw(v))
       })
-      return rawValue as T
     } else if (value instanceof Set) {
-      const rawValue = new Set()
-      value.forEach((v) => {
-        rawValue.add(toRaw(v))
+      const newSet = [...value].map(toRaw)
+
+      value.clear()
+      newSet.forEach((v) => {
+        value.add(v)
       })
-      return rawValue as T
     }
   }
 
